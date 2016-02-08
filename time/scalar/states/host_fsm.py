4cc1eefee5d5a90eba6b6aae6164e92457b7e6ab
András Gyácsok <andras.gyacsok@ericsson.com>
2016-02-08 11:01:38 +0100

    Added small test for FSM

--- time/scalar/states/host_fsm.py
+++ time/scalar/states/host_fsm.py
@@ -3,3 +3,21 @@ class HostFSM(object):
     states file. This class, however, provides callbacks and inner logic.
     """
     pass
+
+
+if __name__ == "__main__":
+    from process_states import Prenatal, Born, GrownUp, LonelyGrownUp, OldBones
+    from process_states import transitions as trans
+    import transitions
+    fsm = HostFSM()
+    machine = transitions.Machine(fsm,
+                                  states=[Born(),
+                                          Prenatal(),
+                                          GrownUp(),
+                                          LonelyGrownUp(),
+                                          OldBones()],
+                                  transitions=trans,
+                                  initial=Prenatal())
+    print fsm.state
+    fsm.ready()
+    print fsm.state
